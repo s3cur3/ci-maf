50e36af5973a3e2036c5f60b932f3531813d3801
Tyler Young <tyler@x-plane.com>
2015-10-20 20:28:55 -0500

    fix (WordPress.org): Nuked premium version (since we should never be building it from this branch)

--- build.sh
+++ build.sh
@@ -10,26 +10,6 @@ find templates -name '.*' -type f -delete
 echo "\n\n\nRunning grunt task..."
 grunt
 
-echo "\n\n\nBeginning premium version build"
-ZIP="ci-modern-accounting-firm.zip"
-echo "Nuking existing ZIP"
-rm ${ZIP}
-
-echo "Creating build directory"
-DIRECTORY="ci-modern-accounting-firm"
-mkdir $DIRECTORY
-
-echo "Copying files to build directory"
-cp -r assets docs lang lib templates style.css README.md screenshot.png 404.php archive-ci-staff.php base-template-landing.php base.php footer-shop.php functions.php Gruntfile.js header-shop.php index.php options.php page.php single-ci-staff.php single.php template-blog.php template-landing.php template-staff.php screenshot.png $DIRECTORY
-
-echo "Zipping build directory"
-zip -r ${ZIP} ${DIRECTORY}
-
-echo "Nuking build directory"
-rm -r ${DIRECTORY}
-
-
-
 echo "\n\n\nBeginning free version build"
 
 ZIP="ci-modern-accounting-firm-free.zip"
