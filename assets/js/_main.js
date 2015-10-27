68be132149ebbf99658f166103d74373bc4e7529
Tyler Young <tyler@x-plane.com>
2015-10-27 17:07:56 -0500

    fix (JS): Fix for JS exception thrown when there's no social media section in the menu bar

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -8,16 +8,18 @@
     // Make sure the header doesn't get smashed up with the menu and social media buttons overlapping
     function showOrHideSocialMedia() {
         var socialBox = $(".post-nav");
-        socialBox.show();
-        var menuBounds = $(".navbar-nav")[0].getBoundingClientRect();
-        var socialBounds = socialBox[0].getBoundingClientRect();
-        var areOverlapping = menuBounds.width >= 1 &&
-            socialBounds.left < menuBounds.right &&
-            socialBounds.top < menuBounds.bottom;
-        if(areOverlapping) {
-            socialBox.hide();
-        } else {
+        if(socialBox.length) {
             socialBox.show();
+            var menuBounds = $(".navbar-nav")[0].getBoundingClientRect();
+            var socialBounds = socialBox[0].getBoundingClientRect();
+            var areOverlapping = menuBounds.width >= 1 &&
+                socialBounds.left < menuBounds.right &&
+                socialBounds.top < menuBounds.bottom;
+            if(areOverlapping) {
+                socialBox.hide();
+            } else {
+                socialBox.show();
+            }
         }
     }
 
